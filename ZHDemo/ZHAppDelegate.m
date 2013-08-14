759705450402b4ba61d31727cad8224cb8f199a5
Edward <lihangqw@126.com>
2013-08-14 11:32:55 +0800

    fix bugs

--- ZHDemo/ZHAppDelegate.m
+++ ZHDemo/ZHAppDelegate.m
@@ -64,18 +64,18 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
     //  }
 
   } else {
-  	// iOS 7
-//    UITabBarItem *collectionBarItem =  [[UITabBarItem alloc] initWithTitle:@"发现" image:[UIImage imageNamed:@"ExploreNormal.png"] selectedImage:[UIImage imageNamed:@"ExploreSelected.png"]];
-//    collectionNav.tabBarItem = collectionBarItem;
-//    
-//    UITabBarItem *answerItem = [[UITabBarItem alloc] initWithTitle:@"消息" image:[UIImage imageNamed:@"NotificationNormal.png"] selectedImage:[UIImage imageNamed:@"NotificationSelected.png"]];
-//    answerNav.tabBarItem = answerItem;
-//    
-//    UITabBarItem *feedItem = [[UITabBarItem alloc] initWithTitle:@"首页" image:[UIImage imageNamed:@"FeedNormal.png"] selectedImage:[UIImage imageNamed:@"FeedSelected.png"]];
-//    feedNav.tabBarItem = feedItem;
-//    
-//    UITabBarItem *userInfoItem = [[UITabBarItem alloc] initWithTitle:@"我" image:[UIImage imageNamed:@"MaleNormal.png"] selectedImage:[UIImage imageNamed:@"MaleSelected.png"]];
-//    userInfoNav.tabBarItem = userInfoItem;
+  	// iOS 7,if error occur here under ioS 6, comment these code block 
+    UITabBarItem *collectionBarItem =  [[UITabBarItem alloc] initWithTitle:@"发现" image:[UIImage imageNamed:@"ExploreNormal.png"] selectedImage:[UIImage imageNamed:@"ExploreSelected.png"]];
+    collectionNav.tabBarItem = collectionBarItem;
+    
+    UITabBarItem *answerItem = [[UITabBarItem alloc] initWithTitle:@"消息" image:[UIImage imageNamed:@"NotificationNormal.png"] selectedImage:[UIImage imageNamed:@"NotificationSelected.png"]];
+    answerNav.tabBarItem = answerItem;
+    
+    UITabBarItem *feedItem = [[UITabBarItem alloc] initWithTitle:@"首页" image:[UIImage imageNamed:@"FeedNormal.png"] selectedImage:[UIImage imageNamed:@"FeedSelected.png"]];
+    feedNav.tabBarItem = feedItem;
+    
+    UITabBarItem *userInfoItem = [[UITabBarItem alloc] initWithTitle:@"我" image:[UIImage imageNamed:@"MaleNormal.png"] selectedImage:[UIImage imageNamed:@"MaleSelected.png"]];
+    userInfoNav.tabBarItem = userInfoItem;
   }
 
   
